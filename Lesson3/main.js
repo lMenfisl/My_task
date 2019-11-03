e9d28041b4255ad73232adda97f063b2067dd774
Андрей Бигун <lmenfisl@gmail.com>
2019-11-03 16:59:58 +0200

    Lesson3_work

--- Lesson3/main.js
+++ Lesson3/main.js
@@ -2,21 +2,12 @@ var colorArray = ["#0404B4", "#FAC46E","#FF0000", "#FAD5BB", "#F2FEFF"];
 var i = 0; 
 
 function nextTap(){
-    var next = document.querySelector('#demo').value;
     document.getElementById("demo").style.backgroundColor= colorArray[i]; 
-    i++;
-    if( i >= colorArray.length-1){
-        i = 0;
-    }
-    next=colorArray[i];
-}
-
-function previousTap(){
-    var next = document.querySelector('#demo').value;
+        i = (i + colorArray.length + 1)%colorArray.length;
     document.getElementById("demo").style.backgroundColor= colorArray[i]; 
-    i--;
-    if( i <= 0){
-        i = colorArray.length-1;
     }
-    next=colorArray[i];
-}
+    function previousTap(){
+        document.getElementById("demo").style.backgroundColor= colorArray[i]; 
+            i = (i + colorArray.length - 1)%colorArray.length;
+        document.getElementById("demo").style.backgroundColor= colorArray[i]; 
+        }
